07ec7cdfb0501102de582d5995bb7ce8b4754bc4
centremetre <luckyminer01@gmail.com>
2024-02-21 15:32:35 +0000

    Refactored Game.kt
    Rewritten all the variables to ones that are needed, changed how accessing questions works

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,10 +1,8 @@
 package org.example
 
-import org.example.question.Question
-import org.example.question.Type
-import org.example.question.Difficulty
-import org.example.question.Category
+import org.example.question.*
 import org.example.tdbapi.ApiHandler
+import org.example.utils.StringUtils.Companion.decodeHtmlEntities
 import org.example.tdbapi.ResponseCode
 
 /*
@@ -23,41 +21,78 @@ So a user only needs to interact with the game class
 
 /**
  * Holds all data related to questions and players game
- * @param numberOfQuestions The amount of questions the game has
- * @param category The category the player wants to play
- * @param difficulty The difficulty of questions the player wants to play
- * @param type The type of questions the player wants to play
+ * @param numberOfQuestions The amount of questions the game has. Default: 10
+ * @param category The category the player wants to play. Default: ANY
+ * @param difficulty The difficulty of questions the player wants to play. Default: ANY
+ * @param type The type of questions the player wants to play. Default: ANY
+ * @throws IllegalArgumentException If the request to the API wasn't successful and the list is not populated correctly
  */
-class Game(val numberOfQuestions: Int,
-           val category: Category,
-           val difficulty: Difficulty,
-           val type: Type
+class Game(val numberOfQuestions: Int = 10,
+           val category: Category = Category.ANY,
+           val difficulty: Difficulty = Difficulty.ANY,
+           val type: Type = Type.ANY
            /*val questionList: List<Question>*/)
 {
-    private val emptyQuestion = Question(Type.BOOLEAN,
-        Difficulty.EASY,
-        Category.ANY,
-        "",
-        "",
-        listOf(""))
-
-    var score: Int = 0
-    var currentQuestion: Question? = null
-    private var questions = List(numberOfQuestions) {emptyQuestion}
+    /**
+     * The final list for the questions of the game
+     */
+    public var questions: List<Question> = listOf()
 
     private var api = ApiHandler()
-    private val jsonParser = JsonParser()
-    public var apiResponse = ParsedResponse(ResponseCode.SUCCESS, listOf(emptyQuestion))
+    private var jsonParser = JsonParser()
 
+    /**
+     * [ResponseCode] of the API call
+     */
+    private var responseCode: ResponseCode
+
+    private var isSuccessful: Boolean = true
+
+    /**
+     * Calls API, gets JSON, parses JSON, puts response into
+     */
     init
     {
-        var json = api.callApi(this)
-        apiResponse = jsonParser.parseIntoParsedResponse(json)
-        questions = apiResponse.results
+        var json: String = api.callApi(this)
+        //json = decodeHtmlEntities(json)
+        val response: ParsedResponse = jsonParser.parseIntoParsedResponse(json)
+        responseCode = response.responseCode
+        setIsSuccessful()
+        if (!isSuccessful()) throw IllegalArgumentException("No questions in list. Reason: ${responseCode.toString()}")
+        questions = response.results
+
+
+
     }
 
-    private fun callApi()
+    /**
+     * Used so that the [] syntax can be used on [questions] since its private
+     */
+    public operator fun get(index: Int): Question
     {
+        return questions[index]
+    }
 
+    public fun getResponseCode(): ResponseCode
+    {
+        return responseCode
+    }
+
+    public fun isSuccessful(): Boolean
+    {
+        return isSuccessful
+    }
+
+    private fun setIsSuccessful()
+    {
+        if (responseCode != ResponseCode.SUCCESS) isSuccessful = false
+    }
+
+    private fun decodeHtmlEncodingInQuestions()
+    {
+        for (q: Question in questions)
+        {
+
+        }
     }
 }
